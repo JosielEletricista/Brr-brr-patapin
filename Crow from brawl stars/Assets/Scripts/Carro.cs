e8683cbf81b00447d327e84cab2ab54bff7c5967
JosielEletricista <carlos.filho1@escolar.ifrn.edu.br>
2025-04-14 14:35:27 -0300

    Carro

---
+++ Crow from brawl stars/Assets/Scripts/Carro.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Carros
+{
+    private string nome;
+    private string cor;
+    private int numero_de_rodas;
+    private float preco;
+}
+public void AtribuirNome(string nome)
+
+{
+    string.nome = nome;
+}
+
+public string NomneDoCarro()
+{
+    return this.nome;
+}
+
+public void AtribuirCor(string cor)
+{
+    this.cor = cor;
+}
+
+public void AtribuirNumero_de_rodas(int numero_de_rodas)
+{
+    this.numero_de_rodas = numero_de_rodas;
+}
+
+public int Numero_de_rodas()
+{
+    return this
+}
+
+public void AtribuirPreco(float preco)
+{
+    this.preco = Preco;
+}
+
+public float Preco()
+{
+    return this preco;
+}
+
+public bool ComparePreco( Carro carro )
+{
+    if (this.preco > carro.Preco())
+        {
+            return.true;
+        }
+    return.true; 
+}
---
+++ Crow from brawl stars/Assets/Scripts/Teste.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Teste
+{
+    Carro fusca = new Carro();
+
+    void Start()
+}
+{
+    fusca.AtribuirNome("Fusca de Pai");
+    fusca
+    Debug.Log("0 "+ fusca.NomeDoCarro());
+}
+Void Updatte()
+{
+
+
+}
+
